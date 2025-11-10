d10b1c2633696c2f3f7425a8bec253ed16821a32
hassan pc <hsn.alidroos.com>
2025-11-10 13:53:49 +0300

    update api service

--- lib/app/services/api/api_service.dart
+++ lib/app/services/api/api_service.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/rendering.dart' show debugPrint;
 import 'package:get/get.dart';
 import 'package:getx_boilerplate/app/core/utils/api_urls.dart';
+import 'package:getx_boilerplate/app/core/utils/http_methods.dart';
 
 
 
@@ -10,6 +12,47 @@ class ApiService extends GetConnect {
     httpClient.timeout = const Duration(seconds: 15);
   }
 
-  Future<Response> getData(String endpoint) => get(endpoint);
-  Future<Response> postData(String endpoint, Map data) => post(endpoint, data);
+  Future<Response> requestServer({
+    required String endpoint,
+    HttpMethods method = HttpMethods.get,
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? query,
+    Map<String, String>? headers,
+  })async{
+   try {
+      late Response response;
+
+      switch (method) {
+        case HttpMethods.get:
+          response = await get(endpoint, query: query, headers: headers);
+          break;
+
+        case HttpMethods.post:
+          response = await post(endpoint, body, headers: headers, query: query);
+          break;
+
+        case HttpMethods.put:
+          response = await put(endpoint, body, headers: headers, query: query);
+          break;
+
+        case HttpMethods.patch:
+          response = await patch(endpoint, body, headers: headers, query: query);
+          break;
+
+        case HttpMethods.delete:
+          response = await delete(endpoint, headers: headers, query: query);
+          break;
+      }
+
+      debugPrint('🔹 [${method.name.toUpperCase()}] $endpoint');
+      debugPrint('🔸 Status: ${response.statusCode}');
+      debugPrint('🔸 Body: ${response.body}');
+
+      return response;
+    } catch (e) {
+      debugPrint('❌ API Error: $e');
+      return Response(statusCode: 500, statusText: e.toString());
+    }
+  }
+  
 }
