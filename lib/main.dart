2129c71606fa7c9f87e1569faad130026a40eb08
hassan pc <hsn.alidroos.com>
2025-11-10 13:13:42 +0300

    update dolders

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 import 'app/services/localization_service.dart';
-import 'app/theme/app_theme.dart';
+import 'app/core/theme/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
